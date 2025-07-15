ba710492e6de440443cbba6bab420af180ff2ef1
erezcohen <erez.cohen@gmail.com>
2025-07-15 16:31:03 +0300

    Add .cursorignore and .nvmrc files; create CLAUDE.md for project guidance; update package.json and package-lock.json with new dependencies

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 
 export function Layout() {
   return (
-    <div className="relative min-h-screen flex flex-col">
+    <div className="relative min-h-screen w-full flex flex-col bg-background">
       <Header />
       <main className="flex-1 container py-6">
         <Outlet />
